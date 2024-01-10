7685a03253b8b4ee72ecc10c82ef5489e5916732
Titi-git <timothe.buchert@hotmail.fr>
2024-01-10 22:13:02 +0100

    EX2.py added without code

---
+++ EX2.py
@@ -0,0 +1,11 @@
+class FIFO:
+    def __init__(self):
+        self.list = []
+        
+class LIFO:
+    def __init__(self):
+        self.list = []
+
+class LILO:
+    def __init__(self):
+        self.list = []
