bf4c16dcfb04cd55b86603b0dca82dd29721f85d
Titi-git <timothe.buchert@hotmail.fr>
2024-01-10 22:13:24 +0100

    EX2.py added with code

--- EX2.py
+++ EX2.py
@@ -1,11 +1,32 @@
 class FIFO:
     def __init__(self):
         self.list = []
+    def push(self, value):
+        self.list.append(value)
+    def pop(self):
+        if len(self.list) > 0:
+            return self.list.pop(0)
+        else:
+            return None
         
 class LIFO:
     def __init__(self):
         self.list = []
+    def push(self, value):
+        self.list.append(value)
+    def pop(self):
+        if len(self.list) > 0:
+            return self.list.pop()
+        else:
+            return None
 
 class LILO:
     def __init__(self):
         self.list = []
+    def push(self, value):
+        self.list.append(value)
+    def pop(self):
+        if len(self.list) > 0:
+            return self.list.pop(0)
+        else:
+            return None
