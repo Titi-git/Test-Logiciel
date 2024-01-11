429b8b5c486efcd9ccc3b1fba1ce8f8208bfec43
Titi-git <timothe.buchert@hotmail.fr>
2024-01-11 11:22:49 +0100

    Modify tests for EX1 prime function

--- EX1_test.py
+++ EX1_test.py
@@ -19,9 +19,16 @@ class TestEX1(unittest.TestCase):
 
     def test_is_prime(self):
         self.assertEqual(EX1.is_prime(1), True)
-        self.assertEqual(EX1.is_prime(2), False)
+        self.assertEqual(EX1.is_prime(2), True)
         self.assertEqual(EX1.is_prime(3), True)
         self.assertEqual(EX1.is_prime(4), False)
+        self.assertEqual(EX1.is_prime(5), True)
+        self.assertEqual(EX1.is_prime(6), False)
+        self.assertEqual(EX1.is_prime(7), True)
+        self.assertEqual(EX1.is_prime(8), False)
+        self.assertEqual(EX1.is_prime(9), False)
+        self.assertEqual(EX1.is_prime(10), False)
+        self.assertEqual(EX1.is_prime(11), True)
 
     def test_is_arithmetic(self):
         self.assertEqual(EX1.is_arithmetic([1, 2, 3, 4, 5]), True)
