79952ddbeeba26c6140bba06965ac269d5c3e038
Titi-git <timothe.buchert@hotmail.fr>
2024-01-12 10:13:56 +0100

    ajout tests unitaires pour partie_gagnee

--- morpion.c
+++ morpion.c
@@ -9,6 +9,7 @@
 //==================================== Tests Definitions ====================================
 void test_initialisation();
 void test_placer_pion();
+void test_partie_gagnee();
 
 //==================================== Fonctions ====================================
 void initialisation(char plateau[N][N]){
@@ -107,7 +108,7 @@ int partie_gagnee(char plateau[N][N]){
 	//test colonne
 	for (int i=0;i<N;i++){
 		for(int j=0;j<N-1;j++){
-			if(plateau[j][i]==plateau[j+1][i] && plateau[i][j]!='_'){
+			if(plateau[j][i]==plateau[j+1][i] && plateau[j][i]!='_'){
 				flag=1;
 			}
 			else{
@@ -121,7 +122,7 @@ int partie_gagnee(char plateau[N][N]){
 			return 1;
 		}
 	}
-	//test diagonale
+	//test diagonale 1
 	for (int i=0;i<N-1;i++){
 		if(plateau[i][i]==plateau[i+1][i+1] && plateau[i][i]!='_'){
 			flag=1;
@@ -133,6 +134,21 @@ int partie_gagnee(char plateau[N][N]){
 	}
 	if(flag==1){
 		if(plateau[0][0]=='X'){printf("\n Le joueur 1 a gagné \n");}
+		else{printf("\n Le joueur 2 a gagné \n");}
+		return 1;
+	}
+	//test diagonale 2
+	for (int i=0;i<N-1;i++){
+		if(plateau[i][N-1-i]==plateau[i+1][N-1-i-1] && plateau[i][N-1-i]!='_'){
+			flag=1;
+		}
+		else{
+			flag=0;
+			break;
+		}
+	}
+	if(flag==1){
+		if(plateau[0][N-1]=='X'){printf("\n Le joueur 1 a gagné \n");}
 		else{printf("\n Le joueur 2 a gagné \n");}
 		return 1;
 	}
@@ -188,6 +204,7 @@ int main(){
 
     CU_add_test(suite, "test_initialisation", test_initialisation);
 	CU_add_test(suite, "test_placer_pion", test_placer_pion);
+	CU_add_test(suite, "test_partie_gagnee", test_partie_gagnee);
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
@@ -247,4 +264,66 @@ void test_placer_pion(){
 	CU_ASSERT(plateau[2][2]=='O');
 }
 
+void test_partie_gagnee(){
+	char plateau[N][N];
+	initialisation(plateau);
+	char signe='X';
+	char buffer[2];
+	buffer[0]='1';
+	buffer[1]='1';
+	placer_pion(plateau,signe,buffer);
+	buffer[0]='2';
+	buffer[1]='1';
+	placer_pion(plateau,signe,buffer);
+	buffer[0]='3';
+	buffer[1]='1';
+	placer_pion(plateau,signe,buffer);
+	printf("ASSERT 1\n");
+	CU_ASSERT(partie_gagnee(plateau)==1);
+	printf("FIN ASSERT 1\n");
+	initialisation(plateau);
+	buffer[0]='1';
+	buffer[1]='1';
+	placer_pion(plateau,signe,buffer);
+	buffer[0]='1';
+	buffer[1]='2';
+	placer_pion(plateau,signe,buffer);
+	buffer[0]='1';
+	buffer[1]='3';
+	placer_pion(plateau,signe,buffer);
+	CU_ASSERT(partie_gagnee(plateau)==1);
+	initialisation(plateau);
+	buffer[0]='1';
+	buffer[1]='1';
+	placer_pion(plateau,signe,buffer);
+	buffer[0]='2';
+	buffer[1]='2';
+	placer_pion(plateau,signe,buffer);
+	buffer[0]='3';
+	buffer[1]='3';
+	placer_pion(plateau,signe,buffer);
+	CU_ASSERT(partie_gagnee(plateau)==1);
+	initialisation(plateau);
+	buffer[0]='1';
+	buffer[1]='3';
+	placer_pion(plateau,signe,buffer);
+	buffer[0]='2';
+	buffer[1]='2';
+	placer_pion(plateau,signe,buffer);
+	buffer[0]='3';
+	buffer[1]='1';
+	placer_pion(plateau,signe,buffer);
+	CU_ASSERT(partie_gagnee(plateau)==1);
+	initialisation(plateau);
+	buffer[0]='1';
+	buffer[1]='1';
+	placer_pion(plateau,signe,buffer);
+	buffer[0]='2';
+	buffer[1]='1';
+	placer_pion(plateau,signe,buffer);
+	buffer[0]='3';
+	buffer[1]='2';
+	placer_pion(plateau,signe,buffer);
+	CU_ASSERT(partie_gagnee(plateau)==-1);
+}
 
